cdb2c79e723cea283c84bdc066b0658074a34593
vdsbenoit <vdsbenoit@gmail.com>
2024-11-05 23:57:00 +0100

    Create GameAvailabilities component
    In order to make the availabilities reactive

---
+++ src/components/GameAvailabilities.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div v-for="(availability, i) in availabilities" :key="i">
+      <ion-badge slot="end" class="ion-no-margin" :color="availability.color">
+        {{ availability.nbAttendants }}
+      </ion-badge>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useAppConfig, useAppSettings } from "@/composables/app"
+import { Game } from "@/types"
+import { computed, defineProps } from "vue"
+
+// Props
+
+const props = defineProps<{
+  game: Game
+}>()
+
+// Composables
+
+const appSettings = useAppSettings()
+const appConfig = useAppConfig()
+
+// Computed data
+
+const maxGameLeaders = computed(() => {
+  if (!appSettings.value) return 2
+  return appSettings.value?.maxGameAttendants
+})
+const attendantSchedule = computed(() => appConfig.value?.attendantSchedule ?? [])
+
+const availabilities = computed(() => {
+  const data = []
+  for (const timeSlot of attendantSchedule.value) {
+    const nbAttendants = props.game.attendants[timeSlot.id].length
+    let color = "success"
+    if (nbAttendants === 0) color = "danger"
+    if (nbAttendants < maxGameLeaders.value) color = "warning"
+    data.push({ nbAttendants, color })
+  }
+  return data
+})
+</script>
+
+<style scoped></style>
