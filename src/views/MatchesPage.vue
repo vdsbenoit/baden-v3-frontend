55bb216faa14369b41c0c16f3990a36ffe713cfb
vdsbenoit <vdsbenoit@gmail.com>
2024-11-06 17:56:19 +0100

    Refactor MatchesPage for new db schema

--- src/views/MatchesPage.vue
+++ src/views/MatchesPage.vue
@@ -6,14 +6,24 @@
       <ion-item color="primary">
         <ion-label>Choisir un horaire</ion-label>
         <ion-select v-model="selectedTime" interface="popover">
-          <ion-select-option v-for="(timing, index) in schedules" :value="index + 1" :key="index">{{ timing.start }} - {{ timing.stop }}</ion-select-option>
+          <ion-select-option v-for="(timeSlot, index) in schedules" :value="index + 1" :key="index">{{ timeSlot.start }} - {{ timeSlot.stop }}</ion-select-option>
         </ion-select>
       </ion-item>
       <ion-item v-if="isFilled" color="warning">
         <ion-label class="ion-text-center">Complet</ion-label>
       </ion-item>
-      <ion-list v-if="selectedTime">
-        <div v-if="matches && matches.size > 0">
+      <div v-if="selectedTime === DEFAULT_TIME_VALUE" class="not-found">
+        <h2 class="ion-text-center ion-align-items-center">Sélectionne un horaire <ion-icon :ios="arrowUpOutline" :md="arrowUpSharp"></ion-icon></h2>
+      </div>
+      <ion-list v-else>
+        <div v-if="isLoadingMatches" class="ion-text-center">
+          <ion-spinner></ion-spinner>
+        </div>
+        <div v-else-if="errorLoadingMatches" class="not-found">
+          <strong class="capitalize">Houston, nous avons une erreur</strong>
+          <ion-text color="error">{{ errorLoadingMatches.message }}</ion-text>
+        </div>
+        <div v-if="matches && matches.length > 0">
           <ion-item v-for="match in matches.values()" :key="match.id" :routerLink="`/match/${match.id}`" class="">
             <ion-badge slot="start" class="ion-no-margin ion-margin-end" color="medium">{{ match.game_id }}</ion-badge>
             <ion-label>
@@ -23,16 +33,10 @@
             <ion-icon v-else color="danger" slot="end" :ios="closeCircle" :md="closeCircleSharp"></ion-icon>
           </ion-item>
         </div>
-        <div v-else-if="isLoading" class="ion-text-center" style="background: transparent">
-          <ion-spinner></ion-spinner>
+        <div v-else class="not-found">
+          <strong class="capitalize">Nous n'avons pas trouvé de matches pour cet horaire...</strong>
         </div>
       </ion-list>
-      <div v-else class="not-found">
-        <h2 class="ion-text-center ion-align-items-center">Sélectionne un horaire <ion-icon :ios="arrowUpOutline" :md="arrowUpSharp"></ion-icon></h2>
-      </div>
-      <div v-if="selectedTime && !isLoading && (!matches || matches.size < 1)" class="not-found">
-        <h2 class="ion-text-center ion-align-items-center">Aucun duel trouvé</h2>
-      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -42,44 +46,35 @@ import { IonContent, IonPage, IonList, IonItem, IonLabel, IonBadge, IonSpinner, 
 import { arrowUpOutline, arrowUpSharp, checkmarkCircle, checkmarkCircleSharp, closeCircle, closeCircleSharp} from "ionicons/icons";
 import HeaderTemplate from "@/components/HeaderTemplate.vue";
 import { computed, ref } from "@vue/reactivity";
-import { getSchedules } from "@/services/settings";
-import { streamTimeMatches, Match } from "@/services/matches";
-import { onMounted } from "vue";
 import RefresherComponent from "@/components/RefresherComponent.vue";
+import { useTimeMatches } from "@/composables/match";
+import { DEFAULT_TIME_VALUE } from "@/constants";
+import { useAppConfig } from "@/composables/app";
+import { Match } from "@/types";
 
 // reactive data
-const selectedTime = ref(0);
-const isLoading = ref(true);
+const selectedTime = ref(DEFAULT_TIME_VALUE);
 
-// lifecycle hooks
-onMounted(() => {
-  setTimeout(() => {
-    isLoading.value = false;
-  }, 5000);
-});
+// Composables
+const appConfig = useAppConfig()
+const {data: matches, pending: isLoadingMatches, error: errorLoadingMatches } = useTimeMatches(selectedTime)
 
 // Computed
 const schedules = computed(() => {
-  console.log("schedules", getSchedules());
-  return getSchedules();
-});
-const matches = computed((): Map<string, Match> | undefined => {
-  return streamTimeMatches(selectedTime.value);
-});
-const isFilled = computed(() => {
-  if (matches.value && matches.value.size > 0) {
-    for (const match of matches.value.values()) {
-      if (match.noScores) continue
-      if (match.winner) continue
-      if (match.draw) continue
-      return false;
-    }
-    return true;
-  }
-  return false;
+  if(!appConfig.value) return []
+  return appConfig.value.playerSchedule
 });
 
-// Watchers
+const isFilled = computed(() => {
+  if (!matches.value || matches.value.length === 0) return false;
+  for (const match of matches.value) {
+    if (match.noScores) continue
+    if (match.winner) continue
+    if (match.draw) continue
+    return false;
+  }
+  return true;
+});
 
 // Methods
 
