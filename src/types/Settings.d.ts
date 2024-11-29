761703fe1216a0a048ea57036fba7de998877ea4
vdsbenoit <vdsbenoit@gmail.com>
2024-11-29 23:35:29 +0100

    Improve documentation

--- src/types/Settings.d.ts
+++ src/types/Settings.d.ts
@@ -5,11 +5,11 @@ import { VueFirestoreDocumentData} from "vuefire"
 
 export type AppSettings = {
   maxGameAttendants: number; // max allowed attendants per game
-  canSetScores: boolean;
-  canSetAnyScores: boolean;
+  canSetScores: boolean; // switch to enable/disable the score registration
+  canSetAnyScores: boolean; // emergency switch to allow any attendant to set scores (bypassing the attendant registration)
   isAttendantRegistrationOpen: boolean; // true when the attendant can register to games
-  isRankingPublic: boolean;
-  showGameAvailabilities: boolean;
+  isRankingPublic: boolean; // make the ranking visible to all
+  showGameAvailabilities: boolean; // show the game availabilities for attendant registration in the game list
 }
 export type RefAppSettings = Ref<VueFirestoreDocumentData<AppSettings> | undefined>
 
