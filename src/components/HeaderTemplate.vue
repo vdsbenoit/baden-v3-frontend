1e8dd888f631d25c335f70f833416d9972590e9f
vdsbenoit <vdsbenoit@gmail.com>
2025-01-05 05:19:06 +0100

    add home button

--- src/components/HeaderTemplate.vue
+++ src/components/HeaderTemplate.vue
@@ -3,13 +3,14 @@
     <ion-toolbar>
       <ion-buttons slot="start">
         <ion-back-button></ion-back-button>
-      </ion-buttons>
-      <ion-buttons slot="start">
         <slot></slot>
       </ion-buttons>
       <ion-title>{{ props.pageTitle }}</ion-title>
       <ion-buttons slot="end">
-        <ion-menu-button color="primary"></ion-menu-button>
+        <ion-button class="ion-no-margin" href="/home" router-direction="root">
+          <ion-icon color="primary" :icon="homeOutline" :md="homeSharp" slot="icon-only"></ion-icon>
+        </ion-button>
+        <ion-menu-button color="primary" :auto-hide="false"></ion-menu-button>
       </ion-buttons>
     </ion-toolbar>
   </ion-header>
@@ -17,6 +18,7 @@
 
 <script setup lang="ts">
 import { IonButtons, IonHeader, IonMenuButton, IonTitle, IonToolbar, IonBackButton } from "@ionic/vue";
+import { homeOutline, homeSharp } from "ionicons/icons";
 import { defineProps } from "vue";
 const props = defineProps(["pageTitle"]);
 </script>
