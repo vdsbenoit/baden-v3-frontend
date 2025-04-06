25cee6c2595fed8676609c50c8479021a328386a
vdsbenoit <vdsbenoit@gmail.com>
2025-04-06 18:24:45 +0200

    Improve UX to select a circuit

--- src/views/GamesPage.vue
+++ src/views/GamesPage.vue
@@ -9,12 +9,10 @@
     <ion-content :fullscreen="true">
       <refresher-component></refresher-component>
       <ion-item color="primary">
-        <ion-label v-if="groupCategoryName" class="ion-text-center">{{ groupCategoryName }}</ion-label>
-        <ion-label v-else class="ion-text-center">Sélectionne un circuit</ion-label>
         <ion-spinner v-if="isLoadingAppConfig"></ion-spinner>
-        <ion-select v-else-if="circuits" v-model="selectedCircuit" interface="popover">
+        <ion-select v-else-if="circuits" v-model="selectedCircuit" interface="popover" placeholder="Sélectionne un circuit">
           <ion-select-option v-for="letter in Object.keys(circuits).sort()" :value="letter" :key="letter">
-            {{ letter }}</ion-select-option
+            {{ letter }} - {{ getGroupCategoryName(letter) }}</ion-select-option
           >
         </ion-select>
         <div v-else-if="errorLoadingConfig">Erreur</div>
@@ -113,12 +111,13 @@ const circuits = computed(() => {
   if (!appConfig.value) return undefined
   return appConfig.value.circuits
 })
-const groupCategoryName = computed(() => {
+const getGroupCategoryName = (circuitLetter: string) => {
   if (!appConfig.value) return undefined
-  if (!selectedCircuit.value) return undefined
-  const selectedgroupCategoryId = appConfig.value.circuits[selectedCircuit.value]
-  return appConfig.value.groupCategories[selectedgroupCategoryId].name
-})
+  if (!circuitLetter || circuitLetter == DEFAULT_CIRCUIT_VALUE) return undefined
+  const groupCategoryId = appConfig.value.circuits[circuitLetter]
+  return appConfig.value.groupCategories[groupCategoryId].name
+}
+
 const pageTitle = computed(() => {
   if (editMode.value) return `Modification des épreuves`
   return "Épreuves"
@@ -158,5 +157,11 @@ const updateGameName = async () => {
   --padding-end: 0px;
   --inner-padding-start: 0px;
   --inner-padding-end: 0px;
+}
+ion-select {
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  --placeholder-opacity: 1;
 }
 </style>
