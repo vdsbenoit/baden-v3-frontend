3ea2057c59376f53487941aec62ddfff1be6d9ca
vdsbenoit <vdsbenoit@gmail.com>
2025-04-11 15:07:51 +0200

    v0.4.6

--- src/app/buildinfo.ts
+++ src/app/buildinfo.ts
@@ -1,5 +1,5 @@
 export const buildInfo = {
-  buildVersion: "0.4.3",
-  buildDate: 1744338553203,
-  buildDateStr: "Fri Apr 11 2025 04:29:13 GMT+0200 (Central European Summer Time)"
+  buildVersion: "0.4.6",
+  buildDate: 1744376806835,
+  buildDateStr: "Fri Apr 11 2025 15:06:46 GMT+0200 (Central European Summer Time)"
 }
